879ff3ff22e5ef3df97a154e0e8b82b895fbb17c
Gavin Caulfield <gavcaul@udel.edu>
2023-12-19 11:07:51 -0500

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,11 +6,29 @@ export default function About() {
   return (
     <div className="gradient_background">
       <div className="wrapper">
-        <div className="box img">
-          <img src="/IMG_9676.jpg" alt="Colton Portrait" />
+        <div className="center-content">
+          <div className="box img">
+            <img src="/IMG_9676.jpg" alt="Colton Portrait" />
+          </div>
         </div>
         <div className="text-group">
-          Colton is gay ?
+        Meet Colton, an individual whose 
+        passion for music is insufferably exorbitant, 
+        centered around a unique taste that many might consider unusual. 
+        Colton's heart beats in sync with the intricate electronic sounds
+         and avant-garde compositions of Aphex Twin, an artist known for 
+         pushing the boundaries of conventional music. While others may 
+         scratch their heads at the unconventional beats and experimental 
+         melodies, Colton ardently defends Aphex Twin's work as a musical 
+         genius. Passionate to the point of exuberance, Colton enthusiastically 
+         shares the intricate nuances of Aphex Twin's discography, often 
+         leaving friends bewildered by the complex and abstract sonic 
+         landscapes. Unfazed by the raised eyebrows, Colton stands proudly as 
+         a fervent advocate for the electronic avant-garde, finding beauty 
+         in the dissonance and chaos that others may perceive as challenging.
+          In Colton's world, Aphex Twin's music isn't just a sound; it's a 
+          captivating journey through the unexplored realms of sonic artistry.
+        
         </div>
       </div>
       <div className="sec">CONTACT: COLTON.CAULFI@GMAIL.COM</div>
