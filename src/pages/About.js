d2d75e56316b319ec4d7aeb2022a93c67ffeb244
Gavin Caulfield <94129469+gavincaul@users.noreply.github.com>
2025-02-20 12:10:42 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,10 +15,7 @@ export default function About() {
         <div className="text-group">
         Colton Caulfield is a trailer and pitch sizzle editor who has cut for independent filmmakers such as Academy Award-Winning 
         filmmaker Dorothy Fadiman, as well as larger advertising companies such as Wheelhouse Creative, Stage Six Media, Village 
-        Roadshow, NEON Rated. Whether it be trailers, sizzles, reels, or roundtables, Colton understands that editing is an art 
-        form that requires delving into the essence of the client's vision and transforming it into an edit that's not only visually 
-        exciting but also holds gripping narrative. <br></br> <br></br>
-        From conceptualization to realization, Colton is committed and excited to bring your story to life.
+        Roadshow, NEON Rated. Whether it be trailers, sizzles, reels, or roundtables, Colton is committed and excited to bring your story to life.
         </div>
       </div>
       <div className="secab">CONTACT: COLTON.CAULFI@GMAIL.COM</div>
